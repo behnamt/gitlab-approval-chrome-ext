2e098cfa864ecf5a272ff97c65285359d6388370
Behnam <behnam.tehrani@turbinekreuzberg.com>
2019-08-23 09:35:45 +0200

    - move stuff to different files - add unassign button - add help in option page

---
+++ js/entry_point.js
@@ -0,0 +1,49 @@
+'use-strict'
+/*
+ * Loads up the settings, and then begins pulling in the merge request approval info.
+ */
+function getSettingsAndStart () {
+    chrome.storage.local.get(null, function (settings) {
+        if (!isNaN(settings['compact-approval'])) {
+            compactApprovals = settings['compact-approval']
+        }
+        if (!isNaN(settings['author'])) {
+            authorEnabled = settings['author']
+        }
+
+        // Begin parsing and injecting views
+        if (window.location.href.indexOf('merge_requests') !== -1) {
+            if (window.location.href.indexOf('groups') !== -1) {
+                // Get all merge requests for a group view
+                if (checkForNewProjects(settings)) {
+                    getGroupProjectIds(getGroupId())
+                        .then(function (groupInformation) {
+                            groupInformation.projects.forEach(function (project) {
+                                if (!(project.path_with_namespace in settings)) {
+                                    cacheProjectId(project.path_with_namespace, project.id)
+                                }
+                            })
+                        }).then(function () {
+                        parseMergeRequestsOnPage(null)
+                    })
+                } else {
+                    // No need to fetch; we have all the required project ids
+                    parseMergeRequestsOnPage(null)
+                }
+            } else if (window.location.href.indexOf('new') !== -1) {
+                // Auto-check remove branch checkbox if setting is enabled
+                if (!isNaN(settings['auto-select-force-remove'])) {
+                    $('#merge_request_force_remove_source_branch').prop('checked', settings['auto-select-force-remove'])
+                }
+            } else if ($.isNumeric(window.location.pathname.split('/')[4])) {
+                addAssignMyselfAsApproverButton(!isNaN(settings['api-key']) ? settings['api-key'] : false)
+            } else {
+                // Get all merge requests for a project view
+                parseMergeRequestsOnPage(getProjectId())
+            }
+        }
+    })
+}
+
+// Begin running
+getSettingsAndStart()
